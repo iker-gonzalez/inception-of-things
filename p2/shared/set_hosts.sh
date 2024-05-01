d892eb3d15a486a9077edece38de67ece55f0f93
System Administrator <ikergpe@gmail.com>
2024-05-01 10:46:24 +0200

    Add set_hosts script

---
+++ p2/shared/set_hosts.sh
@@ -0,0 +1,5 @@
+echo "192.168.56.110 app1.com" | sudo tee -a "/etc/hosts"
+echo "192.168.56.110 app2.com" | sudo tee -a "/etc/hosts"
+echo "192.168.56.110 app3.com" | sudo tee -a "/etc/hosts"
+
+cat /etc/hosts
